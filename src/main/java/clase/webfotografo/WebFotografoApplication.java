e514d372157843d2e95a2c7a3329c369047a8373
Aignaciofonseca <aignaciofonseca@gmail.com>
2019-02-12 13:16:30 -0300

    inicializando aplicación

---
+++ src/main/java/clase/webfotografo/WebFotografoApplication.java
@@ -0,0 +1,14 @@
+package clase.webfotografo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFotografoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebFotografoApplication.class, args);
+	}
+
+}
+
